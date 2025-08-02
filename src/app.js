ea34b4732ca59e825d0cd6ba00be308a4c57c867
Kevin Arnawa <kevin1arnawa@gmail.com>
2025-08-02 14:44:04 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.use(express.json()); // Middleware untuk parsing JSON body
+
+// Rute dasar
+app.get('/', (req, res) => {
+    res.send('Welcome to the E-commerce API!');
+});
+
+// Rute untuk setiap modul akan ditambahkan di sini
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
